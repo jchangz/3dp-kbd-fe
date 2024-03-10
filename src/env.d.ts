04365261401b0289c05f60f9c206a2c46bf41996
Jian Chang <45157140+jchangz@users.noreply.github.com>
2024-03-10 03:56:44 -0600

    [refactor]: three component

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="astro/client" />
+
+interface mxObj {
+  x: number[];
+  y: number[];
+  z: number[];
+}
+interface rowsObj {
+  [key: string]: { length: number; matrix: number[] };
+}
+interface threeObj {
+  [index: string]: {
+    mx: mxObj;
+    rows: rowsObj;
+  };
+}
